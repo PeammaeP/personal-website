3d86e3125468e6c148eba625d8edd23757fd202f
PeamMiiZ <peemmygg@gmail.com>
2024-09-20 20:28:52 +0700

    Add NavLinkComponent to make code flexibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import HeroSection from "./components/hero-section";
+import NavBar from "./components/navBar";
 
 export default function Home() {
   return (
     <main className="flex g-10 p-10 flex-col min-h-screen bg-[#080808]">
-      <div className="container">
+      <header>
+        <NavBar />
+      </header>
+      <div className="container mx-auto px-12 py-10">
         <HeroSection />
       </div>
     </main>
